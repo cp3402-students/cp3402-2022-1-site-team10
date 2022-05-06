05d4cc26ee7a4a6e20388b26b9c36f8e155ee46d
MdouglasJCU <53589460+MdouglasJCU@users.noreply.github.com>
2022-05-06 21:10:19 +1000

    Include footer and header

--- jazz-club/Gallery.php
+++ jazz-club/Gallery.php
@@ -1,4 +1,7 @@
 <?php
+get_header();
+?>
+<?php
     /*
     Template Name: Gallery
     */
@@ -7,4 +10,8 @@
 <?php
 get_template_part( 'template-parts/content', 'page' );
 ?>	
+
+<?php
+get_footer();
+?>
 		
